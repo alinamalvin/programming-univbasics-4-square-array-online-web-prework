1cfd166575823e959cf7bd9c442b4e069e4f9c1b
Alina Kornilova <alinakornilova78@gmail.com>
2019-09-03 01:41:26 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,8 @@
 def square_array(array)
-  # your code here
+  array=([1, 2, 3])
+  array.each do |number|
+    new_number = number ** 2
+    new_array = []
+    new_array.push (new_number)
+    return new_array
 end
